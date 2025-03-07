5d31dd43d72549067c799cd25c2e302a5248bc22
Feihong Hsu <feihong.hsu@gmail.com>
2025-03-07 12:56:36 -0600

    Add polygons

--- polygons.py
+++ polygons.py
@@ -1,6 +1,7 @@
 """
 Every line is connected to the previous line. Line colors cycle through the colors of the rainbow.
 """
+import random
 import itertools
 import pygame
 import pg
@@ -11,24 +12,27 @@ height = 800
 screen = pg.get_screen(width, height)
 
 color_names = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-coords = [(width // 2, height // 2)]
+coords = []
+polygons = []
+
 
 def on_event(event):
-    global coords, colors, width
+    global coords, colors
 
     if event.type == pygame.MOUSEBUTTONDOWN:
         coords.append(pygame.mouse.get_pos())
     elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-        coords = [(width // 2, height // 2)]
-        screen.fill('black')
+        if len(coords) >= 3:
+            polygons.append(coords[:])
+            coords = []
 
 def loop():
-    for width, color_name, (point1, point2) in zip(
-        itertools.count(3), itertools.cycle(color_names), itertools.pairwise(coords)):
-        pygame.draw.line(screen, color_name, point1, point2, width)
+    for color_name, polygon in zip(itertools.cycle(color_names), polygons):
+        pygame.draw.polygon(screen, color_name, polygon)
 
-    point1 = coords[-1]
-    point2 = pygame.mouse.get_pos()
-    pygame.draw.line(screen, 'white', point1, point2)
+    if len(coords) > 0:
+        point1 = coords[-1]
+        point2 = pygame.mouse.get_pos()
+        pygame.draw.line(screen, 'white', point1, point2)
 
-pg.run('Pygame Lines', loop, on_event)
+pg.run('Polygons', loop, on_event)
